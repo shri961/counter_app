090af2e07ca8d16fa1a30b353d8b811feab5391c
shrinivas <shrinivas@LAPTOP-S7C1MOVD>
2021-12-12 14:47:24 +0530

    finshed counter app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,117 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function App() { 
+  const [count,addCount] = useState(1);
+  // const [mulCoun,multCount] = useState(1);
+  const increase = () => {
+  addCount(count + 1);
+
+}
+
+const decrease = () => {
+  addCount(count - 1);
+  
+}
+
+const multiply = () => {
+  addCount(count * 2 );
+  console.log(addCount);
+}
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      backgroundColor:'rgb(50 188 50 / 37%)',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '300%',
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      top: '-15%',
+      
+    }}>
+      
+      
+      <h1 style={{
+        position: 'relative',
+        width: '500px',
+        backgroundColor: '#7fbd7f87',
+        color: 'white',
+        textAlign: 'center',
+        justifyContent: 'center',
+        fontSize: '60px',
+        borderRadius: '20px'
+      }}>COUNTER APP</h1>
+      
+      
+      
+      <p style={{
+        fontSize: '100px',
+        fontFamily: 'Changa',
+        backgroundColor: '#7fbd7f87',
+        height: '1.5em',
+        width: '1.5em',
+        textAlign: 'center'
+      }}>{count}</p>
+
+{/* <p style={{
+        fontSize: '100px',
+        fontFamily: 'Changa',
+        backgroundColor: '#7fbd7f87',
+        height: '1.5em',
+        width: '1.5em',
+        textAlign: 'center'
+      }}>{mulCoun}</p>
+       */}
+      
+      <div className="buttons">
+      
+      
+      
+      <button onClick={increase}
+      style={{
+        fontSize: '60%',
+        position: 'relative',
+        top: '20vh',
+        marginRight: '5px',
+        backgroundColor: 'green',
+        borderRadius: '8%',
+        color: 'white',
+      }}
+      >Increase</button>
+      
+      
+      
+      <button  onClick={decrease}
+      style={{
+        fontSize: '60%',
+        position: 'relative',
+        top: '20vh',
+        marginRight: '5px',
+        backgroundColor: 'green',
+        borderRadius: '8%',
+        color: 'white',
+      }}
+      >decrease</button>
+
+
+<button  onClick={multiply}
+      style={{
+        fontSize: '60%',
+        position: 'relative',
+        top: '20vh',
+        marginRight: '5px',
+        backgroundColor: 'green',
+        borderRadius: '8%',
+        color: 'white',
+      }}
+      >dubble</button>
+      </div>
     </div>
   );
 }
